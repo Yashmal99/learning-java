a255f4190b7d5c6afa18ca4fbc846cb8505eb76f
yashmal99 <malasiyayash99@gmail.com>
2019-09-15 11:53:16 +0530

    define run method

--- multithreadingexample2.java
+++ multithreadingexample2.java
@@ -10,6 +10,17 @@ class NewThread implements Runnable {
 
     @Override
     public void run() {
+    try {
+            for(int i = 5; i > 0; i--) {
+                System.out.println("child Thread: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("child thread interrupted.");
+        }
+        System.out.println("child thread exiting.");
+    }
+}
 
     }
     // This is the entry point for the second thread.
